b9c8ee45343e6908524761f4dfed42b446b8e501
Azambik <71612017+Azambik@users.noreply.github.com>
2021-01-16 16:51:39 -0500

    set up server to include express.js and middle ware for handling arrays json and html files

---
+++ server.js
@@ -0,0 +1,20 @@
+//adding express.js to project so its features can be used.
+const express = require("express");
+
+//setting up a port variable that will set to ether an environment set point or default to 3001
+const PORT = process.env.PORT || 3001;
+//calling an instance of express and naming it app
+const app = express();
+
+//middleware for handling objects with arrays
+app.use(express.urlencoded({ extended: true }))
+//middleware for handling json objects
+app.use(express.json());
+//middle ware to allow access to public folder
+app.use(express.static('public'))
+
+
+//server set to listen for request on port saved in PORT
+app.listen(PORT, () =>{
+    console.log(`API server now on port ${PORT}`)
+})
